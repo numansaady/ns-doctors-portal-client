5d534f50aa58b1e461a2c5b23f5971b9a0161f70
numansaady <numansaady@gmail.com>
2022-05-13 01:27:39 +0600

    google login added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from "../../firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>This is login</h2>
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="text-2xl text-center font-bold">Login</h2>
+          
+          <div class="divider">OR</div>
+          <button 
+          onClick={()=> signInWithGoogle()}
+          class="btn btn-outline">Continue with Google</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
