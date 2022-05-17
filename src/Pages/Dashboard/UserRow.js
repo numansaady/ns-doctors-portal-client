c5b0ddb523da030b1b7e8f40232637dc3c9fae15
numansaady <numansaady@gmail.com>
2022-05-17 17:18:36 +0600

    update

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,28 +1,42 @@
 import React from "react";
 import { toast } from "react-toastify";
 
-const UserRow = ({user, refetch}) => {
-  const {email, role} = user;
+const UserRow = ({ user, refetch }) => {
+  const { email, role } = user;
   const makeAdmin = () => {
     fetch(`http://localhost:5000/user/admin/${email}`, {
       method: "PUT",
-      headers:{
-        authorization: `Bearer ${localStorage.getItem("accessToken")}`
-      }
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
     })
-    .then(res => res.json())
-    .then(data => {
-      refetch();
-      console.log(data);
-      toast.success("Successfully Made an Admin")
-    })
-  }
+      .then((res) => {
+        if(res.status === 403){
+          toast.error("Failed to make an Admin!")
+        }
+        return res.json()
+      })
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast.success("Successfully Made an Admin");
+        }
+      });
+  };
   return (
     <tr>
       <th>1</th>
       <td>{email}</td>
-      <td>{role !== 'admin' && <button onClick={makeAdmin} className="btn btn-xs">Make Admin</button>}</td>
-      <td><button className="btn btn-xs">Remove User</button></td>
+      <td>
+        {role !== "admin" && (
+          <button onClick={makeAdmin} className="btn btn-xs">
+            Make Admin
+          </button>
+        )}
+      </td>
+      <td>
+        <button className="btn btn-xs">Remove User</button>
+      </td>
     </tr>
   );
 };
